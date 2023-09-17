6b7a4227cd6a8c9285ea66d3d8fd3ffb19175e52
Andrey <test@test>
2023-09-17 12:01:50 +0300

    add Task03 formated

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,13 +5,21 @@
 Console.Write("Input integer: ");
 int num = Convert.ToInt32(Console.ReadLine());
 string day;
-if(num == 1) day = "понедельник";
-else if(num == 2) day = "вторник";
-else if(num == 3) day = "среда";
-else if(num == 4) day = "четверг";
-else if(num == 5) day = "пятница";
-else if(num == 6) day = "суббота";
-else if(num == 7) day = "воскресенье";
-else day = "нет такого дня недели";
+if (num == 1)
+    day = "понедельник";
+else if (num == 2)
+    day = "вторник";
+else if (num == 3)
+    day = "среда";
+else if (num == 4)
+    day = "четверг";
+else if (num == 5)
+    day = "пятница";
+else if (num == 6)
+    day = "суббота";
+else if (num == 7)
+    day = "воскресенье";
+else
+    day = "нет такого дня недели";
 
 Console.Write($"{num} -> {day}");
