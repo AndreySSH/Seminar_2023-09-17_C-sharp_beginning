8893eed8c4e22fe322494389344009a9051d8c9a
Andrey <test@test>
2023-09-17 11:25:09 +0300

    Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// 1. Принять число на вход
+// 2. Вычислить его квадрат
+// 3. Вывести результат
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+
+//Console.WriteLine(number + " -> " + square);
+Console.WriteLine($"{number} -> {square}");
