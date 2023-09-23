76a2a0d6fd35a3159a171a89e268bc211a4520e2
Andrey <test@test>
2023-09-23 23:22:18 +0300

    add task10 homework 2

---
+++ Task10_hw02/Program.cs
@@ -0,0 +1,15 @@
+﻿// 10. Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(99 < number && number < 1000)
+{
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine($"{number} -> {secondDigit}");
+}
+else Console.WriteLine("Input number is not three digit.");
