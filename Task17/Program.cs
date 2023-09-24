4bbbf6a79528cf0d933e95c8b7c0ccd2acf0b54d
Andrey <test@test>
2023-09-24 10:26:10 +0300

    add task17 example

---
+++ Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+int Quater(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.WriteLine("Input point coords");
+
+Console.Write("X: ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+int quater = Quater(xCoord, yCoord);
+
+string result = quater > 0
+                    ? $"Quater -> {quater}"
+                    : "The point is on one of the axes";
+
+Console.Write(result);
