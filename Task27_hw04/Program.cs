6d25cfc5eabd8579f8b13dc3e9c2e66d0a8b5350
Andrey <test@test>
2023-10-02 18:59:06 +0300

    add task 27 homehork 4

--- Task27_hw04/Program.cs
+++ Task27_hw04/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfNumberDigits(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+        int temp = num % 10;
+        result += temp;
+        num /= 10;
+    }
+    return result;
+}
+
+int number = Prompt("Input number: ");
+if(number < 0)
+    number *= -1;
+
+int result = SumOfNumberDigits(number);
+Console.WriteLine($"{number} -> {result}");
