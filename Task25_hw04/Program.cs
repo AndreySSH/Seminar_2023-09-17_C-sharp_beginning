b23b25fc3029fb16f8137f4a74daded436c5fada
Andrey <test@test>
2023-10-02 18:48:51 +0300

    add task 25 homehork 4

--- Task25_hw04/Program.cs
+++ Task25_hw04/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два
+// числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Pow(int number, int pow)
+{
+    int result = number;
+    for(int i = 1; i < pow; i++)
+        result *= number;
+    return result;
+}
+
+int numberA = Prompt("Input number A: ");
+int numberB = Prompt("Input number B: ");
+if(numberB < 1)
+{
+    Console.WriteLine($"{numberB} is not natural number!");
+    return;
+}
+
+int result = Pow(numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {result}");
