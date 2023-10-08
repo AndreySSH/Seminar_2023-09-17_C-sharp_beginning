5aaf3d40e8dc464dfef787fcc4223a728c64aa66
Andrey <test@test>
2023-10-09 02:53:03 +0300

    task 47 homework 7 done for autotest

--- Task47_hw07/Program.cs
+++ Task47_hw07/Program.cs
@@ -5,47 +5,80 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-internal partial class Program
-{
-    private static int Prompt(string message)
-    {
-        Console.Write(message);
-        return Convert.ToInt32(Console.ReadLine());
-    }
-    
-    public static double[,] CreateRandomMatrix(int m, int n,
-                                               int minLimitRandom,
-                                               int maxLimitRandom)
-    {
-        double[,] arr = new double[m, n];
-        Random rnd = new Random();
+using System;
 
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                arr[i, j] = rnd.NextDouble() * (maxLimitRandom - minLimitRandom) + minLimitRandom;
-            
-        return arr;
+public class Answer {
+  public static double[, ] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom) {
+      // Введите свое решение ниже
+      double[,] arr = new double[m, n];
+      Random rnd = new Random();
+
+      for(int i = 0; i < m; i++)
+      {
+          for(int j = 0; j < n; j++)
+          {
+              arr[i, j] = rnd.NextDouble() * (maxLimitRandom - minLimitRandom) + minLimitRandom;
+          }
+      }
+      return arr;
 
 
-    }
+  }
 
-    public static void Print2DArrayDouble(double[,] matrix)
-    {
-        for (int i = 0; i < matrix.GetLength(0); i++)
+  public static void PrintArray(double[, ] matrix) {
+      // Введите свое решение ниже
+      for(int i = 0; i < matrix.GetLength(0); i++)
+      {
+          for(int j = 0; j < matrix.GetLength(1) - 1; j++)
+          {
+              Console.Write($"{matrix[i, j]:F2}\t");
+          }
+          Console.WriteLine($"{matrix[i, matrix.GetLength(1) - 1]:F2}");
+      }
+  }
+
+  // Не удаляйте и не меняйте метод Main! 
+  public static void Main(string[] args) {
+    int m, n, minLimitRandom, maxLimitRandom;
+
+    if (args.Length >= 4) {
+      m = int.Parse(args[0]);
+      n = int.Parse(args[1]);
+      minLimitRandom = int.Parse(args[2]);
+      maxLimitRandom = int.Parse(args[3]);
+
+      double[,] array = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+
+        // Выберем случайные индексы трех элементов матрицы array
+        int row1 = new Random().Next(0, m);
+        int col1 = new Random().Next(0, n);
+        int row2 = new Random().Next(0, m);
+        int col2 = new Random().Next(0, n);
+        int row3 = new Random().Next(0, m);
+        int col3 = new Random().Next(0, n);
+
+        // Проверяем, являются ли выбранные элементы дробными числами
+        bool isFractional1 = (array[row1, col1] % 1) != 0;
+        bool isFractional2 = (array[row2, col2] % 1) != 0;
+        bool isFractional3 = (array[row3, col3] % 1) != 0;
+
+        // Если два из трех элементов не являются дробными числами, то бросаем исключение
+        if ((isFractional1 && isFractional2) || (isFractional1 && isFractional3) || (isFractional2 && isFractional3))
         {
-            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
-                Console.Write($"{matrix[i, j]:F2}\t");
-            
-            Console.WriteLine($"{matrix[i, matrix.GetLength(1) - 1]:F2}");
+            Console.WriteLine("Все ок!");
         }
+        else
+        {
+            throw new Exception("Выбранные элементы не содержат по крайней мере два дробных числа.");
+        }
+    } else {
+      m = 3;
+      n = 4;
+      minLimitRandom = -10;
+      maxLimitRandom = 10;
+      
+      double[, ] result = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+      PrintArray(result);
     }
-    private static void Main(string[] args)
-    {
-        int m = Prompt("Input rows number m: ");
-        int n = Prompt("Input columns number n: ");
-
-        double[,] array = CreateRandomMatrix(m, n, -10, 10);
-        
-        Print2DArrayDouble(array);
-    }
+  }
 }
