282ccbdaee2f5e1288e880cf8208c7d1c5ebcd52
Andrey <test@test>
2023-10-09 01:05:32 +0300

    task 41 homework 6 done

---
+++ Task41_hw06/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+internal partial class Program
+{
+    private static int[] PromptMulti()
+    {
+        Console.WriteLine("Input multiple numbers");
+        string[] numbersString = Console.ReadLine().Split(',',' ');
+        int[] numbers = new int[numbersString.Length];
+
+        for (int i = 0; i < numbersString.Length; i++)
+            numbers[i] = Convert.ToInt32(numbersString[i]);
+
+        return numbers;
+    }
+
+    private static int CalcPositives(int[] arr)
+    {
+        int result = 0;
+        for (int i = 0; i < arr.Length; i++)
+            if(arr[i] > 0)
+                result++;
+
+        return result;
+    }
+    private static void Main(string[] args)
+    {
+        int[] array = PromptMulti();
+        System.Console.WriteLine($"Numbers > 0  ==>>  {CalcPositives(array)}");
+    }
+}
