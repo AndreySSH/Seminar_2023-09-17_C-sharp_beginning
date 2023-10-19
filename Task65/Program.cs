a23051b76610f04cdffa819a8e1d3c012a2f706e
Andrey <test@test>
2023-10-19 23:43:26 +0300

    add tasks 63,65,67,69 last seminar

---
+++ Task65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+using System;
+
+void PrintNaturalNumbersRange(int n, int m)
+{
+    if(n > m)
+    {
+        System.Console.Write(n + " ");
+        PrintNaturalNumbersRange(n - 1, m);
+    }
+    else if (n < m)
+    {
+        System.Console.Write(n + " ");
+        PrintNaturalNumbersRange(n + 1, m);
+    }
+    else
+    {
+        System.Console.WriteLine(n);
+    }
+}
+
+System.Console.WriteLine("Input first natural number: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second natural number: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNumbersRange(numberN, numberM);
---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+using System;
+
+int PrintSumOfNumberDigits(int num) // 453 45 4 0
+{
+    if(num == 0) return 0;
+    return num % 10 + PrintSumOfNumberDigits(num / 10); // 453 45 4
+    // 4%10 + 45%10 + 453%10 + 0 = 12
+}
+
+System.Console.WriteLine("Input natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = PrintSumOfNumberDigits(number);
+System.Console.WriteLine($"{number} -> {result}");
